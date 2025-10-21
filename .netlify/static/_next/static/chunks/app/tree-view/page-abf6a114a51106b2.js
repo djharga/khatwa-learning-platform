1d6a32878f7f3778ddce05b08f29a670eee6e7c9
khatwa-platform <khatwa.platform@example.com>
2025-10-21 13:28:13 +0200

    تحديث إعدادات Netlify

--- .netlify/static/_next/static/chunks/app/tree-view/page-abf6a114a51106b2.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[930],{38818:function(e,n,s){Promise.resolve().then(s.t.bind(s,231,23)),Promise.resolve().then(s.bind(s,64078))}},function(e){e.O(0,[5343,231,8211,4182,4078,2971,7023,1744],function(){return e(e.s=38818)}),_N_E=e.O()}]);
